9b0ab3eda8b203fadea0a2bec2f1856bcef7bd30
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-06-06 20:36:17 +0300

    updates to module_raja

--- pyfibot/modules/module_raja.py
+++ pyfibot/modules/module_raja.py
@@ -2,6 +2,9 @@
 import requests
 import json
 from datetime import datetime, timedelta
+import re
+
+rg = re.compile('(\\d+).*?(\\d+).*?(\\d+)', re.IGNORECASE | re.DOTALL)
 
 
 def get_data():
@@ -14,24 +17,36 @@ def get_data():
         return
 
 
-def make_string(data, direction):
-    updateTime = datetime.now() + timedelta(days=2)
-    for k, v in data[direction].iteritems():
-        up = datetime.strptime(v['Update'], 'Update time: %m/%d/%Y at %I:%M:%S %p')
-        up = up.replace(hour=up.hour - 1)
-        if up < updateTime:
-            updateTime = up
+def make_string(data, to_russia):
+    def get_updatetime(value):
+        updatetime = datetime.now() + timedelta(days=2)
+        for k, v in value.iteritems():
+            up = datetime.strptime(v['Update'], 'Update time: %m/%d/%Y at %I:%M:%S %p')
+            up = up.replace(hour=up.hour - 1)
+            if up < updatetime:
+                updatetime = up
+        return updatetime
 
-    nuijamaa = u'Nuijamaa: %s' % (data[direction]['NuijBrus']['Time'])
-    sveto = u'Sveto: %s' % (data[direction]['ImatSvet']['Time'])
-    vaalimaa = u'Vaalimaa: %s' % (data[direction]['VaalTorf']['Time'])
+    def get_wait(value):
+        m = rg.search(value)
+        hours = int(m.group(2))
+        minutes = int(m.group(3))
 
-    if direction == 'ToRussia':
-        return u'Venäjälle: %s, %s, %s (%s)' % (nuijamaa, sveto, vaalimaa, updateTime.strftime('%H:%M'))
-    return u'  Suomeen: %s, %s, %s (%s)' % (nuijamaa, sveto, vaalimaa, updateTime.strftime('%H:%M'))
+        if hours > 0:
+            return '%i hours, %i minutes' % (hours, minutes)
+        return '%i minutes' % (minutes)
+
+    updatetime = get_updatetime(data)
+    nuijamaa = u'Nuijamaa: %s' % (get_wait(data['NuijBrus']['Time']))
+    sveto = u'Sveto: %s' % (get_wait(data['ImatSvet']['Time']))
+    vaalimaa = u'Vaalimaa: %s' % (get_wait(data['VaalTorf']['Time']))
+
+    if to_russia:
+        return u'Venäjälle: %s, %s, %s (%s)' % (nuijamaa, sveto, vaalimaa, updatetime.strftime('%H:%M'))
+    return u'  Suomeen: %s, %s, %s (%s)' % (nuijamaa, sveto, vaalimaa, updatetime.strftime('%H:%M'))
 
 
 def command_raja(bot, user, channel, args):
     data = get_data()
-    bot.say(channel, make_string(data, 'ToRussia'))
-    bot.say(channel, make_string(data, 'ToFinland'))
+    bot.say(channel, make_string(data['ToRussia'], True))
+    bot.say(channel, make_string(data['ToFinland'], False))
