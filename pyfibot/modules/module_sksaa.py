b9c7c96fb2c94c9702326e15cca73c3f94e65f62
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-06-28 17:02:24 +0300

    added support for multiple sensors in sksaa

--- pyfibot/modules/module_sksaa.py
+++ pyfibot/modules/module_sksaa.py
@@ -5,23 +5,27 @@ import requests
 userid = None
 token = None
 url = None
+sensors = None
 
 
 def init(bot):
     global userid
     global token
     global url
+    global sensors
     config = bot.config.get("module_sksaa", {})
     userid = config.get("userid", "")
     token = config.get("token", "")
     url = config.get("url", "")
+    sensors = config.get("sensors", "")
 
 
 def command_sksaa(bot, user, channel, args):
-    print(userid, token, url)
-    if userid and token and url:
-        data = requests.get(url, params={'user': userid, 'token': token}).json()['data'][0]['fields']
-        tempStr = 'Or4: %.1f °c (%s)' % (data['last_temperature'], data['last_temperature_time'][:16].encode('utf-8'))
-        return bot.say(channel, tempStr)
-#    return None
+    if userid and token and url and sensors:
+        data = requests.get(url, params={'user': userid, 'token': token}).json()
+        sensordata = []
+        for s in data['data']:
+            if s['pk'] in sensors:
+                sensordata.append(u'%s: %.1f°C' % (s['fields']['name'], s['fields']['last_temperature']))
+        return bot.say(channel, 'Or4: %s' % ', '.join(sensordata))
     return bot.say(channel, '".sksaa" toistaiseksi pois käytöstä, kokeile ".saa"')
