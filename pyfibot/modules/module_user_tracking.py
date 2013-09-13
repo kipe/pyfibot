6b0c91969d0eee822201fa478250535ee2eff682
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-09-13 19:02:09 +0300

    added auto-opping -system, added functions to get user data and user selectors from hostmask, simplifying the code a bit

--- pyfibot/modules/module_user_tracking.py
+++ pyfibot/modules/module_user_tracking.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals, print_function, division
 import sqlite3
 import os
 from datetime import datetime
+import logging
+
+log = logging.getLogger('user_tracking')
 
 
 class UserSQL:
@@ -19,6 +22,7 @@ class UserSQL:
         channel_db = os.path.join(self.db_dir, channel + '.db')
         create_db = not os.path.exists(channel_db)
         self.conn = sqlite3.connect(channel_db, detect_types=sqlite3.PARSE_DECLTYPES)
+        self.conn.row_factory = sqlite3.Row
         self.c = self.conn.cursor()
 
         if create_db:
@@ -52,48 +56,80 @@ class UserSQL:
         self.conn.commit()
         self.conn.close()
 
+    def _get_user(self, user):
+        selector, data = self._get_user_selector(user)
+        sql = 'SELECT * FROM users WHERE %s LIMIT 1;' % selector
+        self.c.execute(sql, data)
+        return self.c.fetchone()
+
+    def _get_user_selector(self, user):
+        nick = getNick(user)
+        try:
+            ident = getIdent(user)
+        except:
+            ident = None
+        try:
+            host = getHost(user)
+        except:
+            host = None
+
+        if ident and host:
+            sql = 'nick = ? AND ident = ? AND host = ?'
+            data = (nick, ident, host)
+        elif ident:
+            sql = 'nick = ? AND ident = ?'
+            data = (nick, ident)
+        else:
+            sql = 'nick = ?'
+            data = (nick, )
+        return sql, data
+
     def _get_alternative_nicks(self, user):
         '''Fetches users alternative nicks from database. Returns list of nicks.'''
-        nick = getNick(user)
-        ident = getIdent(user)
-        host = getHost(user)
+        selector, data = self._get_user_selector(user)
 
-        sql = 'SELECT alternative_nicks FROM users WHERE nick = ? AND ident = ? AND host = ? LIMIT 1;'
-        data = (nick, ident, host)
+        sql = 'SELECT alternative_nicks FROM users WHERE %s LIMIT 1;' % (selector)
         self.c.execute(sql, data)
         row = self.c.fetchone()
         if row:
-            return filter(None, row[0].split(','))
+            return filter(None, row[str('alternative_nicks')].split(','))
         return []
 
-    def update_user(self, user, channel, event, message=None, spoke=False):
+    def update_user(self, bot, user, channel, event, message=None, spoke=False):
         '''Updates user data to database.'''
-        nick = getNick(user)
-        ident = getIdent(user)
-        host = getHost(user)
+        selector, selector_data = self._get_user_selector(user)
         now = datetime.now()
 
         # create connection and create user, if doesn't already exist
         self._get_conn(channel, user)
 
         # update last event and last seen
-        sql = 'UPDATE users SET last_event = ?, last_seen = ? WHERE nick = ? AND ident = ? AND host = ? LIMIT 1;'
-        data = (event, now, nick, ident, host)
+        sql = 'UPDATE users SET last_event = ?, last_seen = ? WHERE %s LIMIT 1;' % (selector)
+        data = (event, now) + selector_data
         self.c.execute(sql, data)
 
         # if message is specified, update it
         if message:
-            sql = 'UPDATE users SET last_message = ? WHERE nick = ? AND ident = ? AND host = ? LIMIT 1;'
-            data = (message, nick, ident, host)
+            sql = 'UPDATE users SET last_message = ? WHERE %s LIMIT 1;' % (selector)
+            data = (message,) + selector_data
             self.c.execute(sql, data)
 
         # if the user spoke, update last_spoke
         if spoke:
-            sql = 'UPDATE users SET last_spoke = ? WHERE nick = ? AND ident = ? AND host = ? LIMIT 1;'
-            data = (now, nick, ident, host)
+            sql = 'UPDATE users SET last_spoke = ? WHERE %s LIMIT 1;' % (selector)
+            data = (now,) + selector_data
             self.c.execute(sql, data)
 
+        if event == 'join':
+            sql = 'SELECT autoop, autovoice FROM users WHERE %s LIMIT 1;' % (selector)
+            self.c.execute(sql, selector_data)
+            row = self.c.fetchone()
+            if row[str('autoop')]:
+                bot.mode(channel, True, 'o', user=nick)
+                log.info('user %s autoopped on %s' % (user, channel))
+
         self._close_conn()
+        log.debug('user %s updated' % (user))
 
     def kicked(self, kickee, channel, kicker, message):
         '''Handle user being kicked.'''
@@ -101,20 +137,24 @@ class UserSQL:
         # Seems like needs a working whois, as botcore.userKicked receives only nick from kickee
         # Kicker however is an user -object...
         now = datetime.now()
+        selector, selector_data = self._get_user_selector(user)
         # can't create user
         self._get_conn(channel)
         event = 'kicked by %s [%s]' % (kicker, message)
         # for now, we need to update based on nick
-        sql = 'UPDATE users SET last_event = ?, last_seen = ? WHERE nick = ? LIMIT 1;'
-        data = (event, now, kickee)
+        sql = 'UPDATE users SET last_event = ?, last_seen = ? WHERE %s LIMIT 1;' % selector
+        data = (event, now) + selector_data
         self.c.execute(sql, data)
         self._close_conn()
 
+        log.info('user %s kicked from %s by %s [%s]' % (kickee, channel, kicker, message))
+
     def nick_change(self, user, newnick):
         '''Handle user nick change.'''
-        nick = getNick(user)
-        ident = getIdent(user)
-        host = getHost(user)
+        # nick = getNick(user)
+        # ident = getIdent(user)
+        # host = getHost(user)
+        selector, selector_data = self._get_user_selector(user)
 
         now = datetime.now()
         # loop through the networks databases and change nick to every db
@@ -130,16 +170,15 @@ class UserSQL:
                 # add old nick to alternative nicks
                 alternative_nicks.append(nick)
 
-                sql = 'UPDATE users SET nick = ?, last_event = ?, last_seen = ?, alternative_nicks = ? WHERE nick = ? AND ident = ? AND host = ? LIMIT 1;'
-                data = (newnick, 'nick_change', now, ','.join(alternative_nicks), nick, ident, host)
+                sql = 'UPDATE users SET nick = ?, last_event = ?, last_seen = ?, alternative_nicks = ? WHERE %s LIMIT 1;' % selector
+                data = (newnick, 'nick_change', now, ','.join(alternative_nicks)) + selector_data
                 self.c.execute(sql, data)
                 self._close_conn()
+        log.debug('user %s is now known as %s' % (user, newnick))
 
     def user_quit(self, user, message):
         '''Handle user quitting.'''
-        nick = getNick(user)
-        ident = getIdent(user)
-        host = getHost(user)
+        selector, selector_data = self._get_user_selector(user)
 
         now = datetime.now()
         # loop through the networks databases and change nick to every db
@@ -147,10 +186,11 @@ class UserSQL:
             if f.endswith('.db'):
                 # DON'T create user here, we don't want it on every database of network
                 self._get_conn(f.strip('.db'))
-                sql = 'UPDATE users SET last_message = ?, last_event = ?, last_seen = ? WHERE nick = ? AND ident = ? AND host = ? LIMIT 1;'
-                data = (message, 'quit', now, nick, ident, host)
+                sql = 'UPDATE users SET last_message = ?, last_event = ?, last_seen = ? WHERE %s LIMIT 1;' % selector
+                data = (message, 'quit', now) + selector_data
                 self.c.execute(sql, data)
                 self._close_conn()
+        log.debug('user %s quit [%s]' % (user, message))
 
     def find_nick(self, bot, nick, channel):
         '''Finds row by nick from database.'''
@@ -170,12 +210,25 @@ class UserSQL:
             self.c.execute(sql, data)
             row = self.c.fetchone()
             if row:
-                alternative_nicks = filter(None, row[9].split(','))
+                alternative_nicks = filter(None, row[str('alternative_nicks')].split(','))
                 if nick not in alternative_nicks:
                     row = None
 
         self._close_conn()
         return row, alternative
+
+    def set_autoop(self, user, channel, state):
+        reval = None
+        selector, selector_data = self._get_user_selector(user)
+
+        self._get_conn(channel)
+        sql = 'UPDATE users SET autoop = ? WHERE autoop = ? AND %s LIMIT 1;' % (selector)
+        self.c.execute(sql, (state, not state) + selector_data)
+        reval = self.c.rowcount
+        if reval:
+            reval = self._get_user(user)
+        self._close_conn()
+        return reval
 
 
 def handle_privmsg(bot, user, channel, message):
@@ -184,18 +237,18 @@ def handle_privmsg(bot, user, channel, message):
         return
 
     s = UserSQL(bot)
-    s.update_user(user, channel, 'message', message, True)
+    s.update_user(bot, user, channel, 'message', message, True)
 
 
 def handle_userJoined(bot, user, channel):
     s = UserSQL(bot)
-    s.update_user(user, channel, 'join')
+    s.update_user(bot, user, channel, 'join')
 
 
 def handle_userLeft(bot, user, channel, message):
     s = UserSQL(bot)
     if channel is not None:
-        s.update_user(user, channel, 'left', message)
+        s.update_user(bot, user, channel, 'left', message)
     else:
         s.user_quit(user, message)
 
@@ -219,7 +272,9 @@ def command_lastseen(bot, user, channel, args):
     row, alternative = s.find_nick(bot, args, channel)
     msg = 'I have no idea who %s is. At least I haven\'t seen him/her on %s.' % (args, channel)
     if row:
-        msg = '%s!%s@%s was last seen at %s (%s)' % (row[0], row[1], row[2], row[6].strftime('%d.%m.%y %H:%M'), row[5])
+        msg = '%s!%s@%s was last seen at %s (%s)' % (
+            row[str('nick')], row[str('ident')], row[str('host')],
+            row[str('last_seen')].strftime('%d.%m.%y %H:%M'), row[str('last_event')])
     return bot.say(channel, msg)
 
 
@@ -232,5 +287,33 @@ def command_lastspoke(bot, user, channel, args):
     row, alternative = s.find_nick(bot, args, channel)
     msg = 'I have no idea who %s is. At least I haven\'t seen him/her on %s.' % (args, channel)
     if row:
-        msg = '%s!%s@%s last spoke at %s' % (row[0], row[1], row[2], row[4].strftime('%d.%m.%y %H:%M'))
+        msg = '%s!%s@%s last spoke at %s' % (row[str('nick')], row[str('ident')], row[str('host')], row[str('last_spoke')].strftime('%d.%m.%y %H:%M'))
+    return bot.say(channel, msg)
+
+
+def command_set_autoop(bot, user, channel, args):
+    args = args.strip()
+    if not isAdmin(user) or not args:
+        return
+
+    msg = 'No user found with "%s".' % (args)
+    s = UserSQL(bot)
+    row = s.set_autoop(args, channel, True)
+    if row:
+        msg = 'Auto-opping %s!%s@%s on %s.' % (row[str('nick')], row[str('ident')], row[str('host')], channel)
+        log.info(msg)
+    return bot.say(channel, msg)
+
+
+def command_remove_autoop(bot, user, channel, args):
+    args = args.strip()
+    if not isAdmin(user) or not args:
+        return
+
+    msg = 'No user found with "%s" (or not auto-opped).' % (args)
+    s = UserSQL(bot)
+    row = s.set_autoop(args, channel, False)
+    if row:
+        msg = 'Removed auto-op from %s!%s@%s on %s.' % (row[str('nick')], row[str('ident')], row[str('host')], channel)
+        log.info(msg)
     return bot.say(channel, msg)
