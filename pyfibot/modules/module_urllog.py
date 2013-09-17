8848b1432b64cf0df745d7914a755cce683e1415
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-09-17 18:22:36 +0300

    added command to search urls

--- pyfibot/modules/module_urllog.py
+++ pyfibot/modules/module_urllog.py
@@ -6,19 +6,27 @@ import os
 from modules import module_urltitle
 
 
+db = os.path.join('databases', 'urllog.db')
+
+
 def init(bot):
     module_urltitle.init(bot)
 
 
-def handle_url(bot, user, channel, url, msg):
-    ''' Logs urls seen in channels to 'databases/urllog.db' '''
-    n = datetime.datetime.now()
-    db = os.path.join('databases', 'urllog.db')
-    # check if the database exists
-    create_db = not os.path.exists(db)
+def __get_conn():
     conn = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
+    return conn, c
+
+
+def handle_url(bot, user, channel, url, msg):
+    ''' Logs urls seen in channels to 'databases/urllog.db' '''
+    global db
+    n = datetime.datetime.now()
+    # check if the database exists
+    create_db = not os.path.exists(db)
+    conn, c = __get_conn()
 
     # if database doesn't exist, create table
     if create_db:
@@ -84,3 +92,33 @@ def handle_url(bot, user, channel, url, msg):
     c.close()
     conn.commit()
     conn.close()
+
+
+def command_find_url(bot, user, channel, args):
+    global db
+    args = args.strip()
+    if not args:
+        return bot.say(channel, 'I need search parameters.')
+
+    search_str = args.replace('*', '%%')
+
+    if not os.path.exists(db):
+        return bot.say(channel, 'I haven\'t logged any urls yet.')
+
+    sql = 'SELECT * FROM urls WHERE channels LIKE ? AND (url LIKE ? OR title LIKE ?);'
+
+    conn, c = __get_conn()
+    c.execute(sql, ('%%%s%%' % channel, search_str, search_str))
+    rows = c.fetchall()
+    c.close()
+    conn.close()
+
+    if not rows:
+        return bot.say(channel, 'No search results.')
+
+    if len(rows) > 1:
+        return bot.say(channel, 'Found multiple results, please redefine your search string.')
+
+    row = rows[0]
+
+    return bot.say(channel, '%s <%s>' % (row[str('title')], row[str('url')]))
